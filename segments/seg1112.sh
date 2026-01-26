3710f35c79e413492ed8629143a57a3b83fb4bec
Riverxia <riverdalecoder@outlook.com>
2026-01-26 16:53:19 -0500

    testing

--- segments/seg1112.sh
+++ segments/seg1112.sh
@@ -1,2 +1,2 @@
 fastfetch --config /home/river/.config/fastfetch/segments/seg11.jsonc --pipe false| render | rev | cut -c 6- | rev | tr -d '\n' 
-fastfetch --config /home/river/.config/fastfetch/segments/seg22.jsonc --pipe false| render 
+fastfetch --config /home/river/.config/fastfetch/segments/seg12.jsonc --pipe false| render 
