c584f3e5f2a757adbd20765dcb8002db8f34ee7e
Riverxia <riverdalecoder@outlook.com>
2026-01-26 16:55:45 -0500

    something

---
+++ segments/seg1314.sh
@@ -0,0 +1,2 @@
+fastfetch --config /home/river/.config/fastfetch/segments/seg13.jsonc --pipe false| render | rev | cut -c 6- | rev | tr -d '\n' 
+fastfetch --config /home/river/.config/fastfetch/segments/seg14.jsonc --pipe false| render 
