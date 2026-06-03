b011ddb15f63c0b8686103a81e7b0302670f385a
Riverxia <riverdalecoder@outlook.com>
2026-06-02 21:51:57 -0400

    (acm) Currently Listening To: mfp -

--- segments/seg1516.sh
+++ segments/seg1516.sh
@@ -1,2 +1,2 @@
 fastfetch --config /home/river/.config/fastfetch/segments/seg15.jsonc --pipe false| render | rev | cut -c 6- | rev | tr -d '\n' 
-python3 /home/river/.config/fastfetch/segments/microterm.py
+fastfetch --config /home/river/.config/fastfetch/segments/seg16.jsonc | render
